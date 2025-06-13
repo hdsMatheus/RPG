f0610e52d13908908db6cf29d11d1fc622a7287f
Obara <nickobara@hotmail.com>
2025-06-13 18:32:17 -0300

    Modifiquei a main pra testar a função de imprimir status

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -9,23 +9,26 @@
 #include "../funcoes.h"
 #include "../Entidades/Esqueleto.h"
 
+
 using namespace std;
 int main(){
-    cout << "Insira seu nome: ";
-    string nome;
-    cin >> nome;
+    // cout << "Insira seu nome: ";
+    // string nome;
+    // cin >> nome;
     Entidade *m = new Mago();
     Entidade *g = new Guerreiro();
-    Jogador p1{nome, m};
-    cout << "\nNome: " << p1.get_jogador_nome() << endl << "Classe: " << p1.get_jogador_classe_nome() << endl;
-    Graveto_do_aprendiz item1;
-    Espada_enferrujada item2;
+    Jogador p1{"Jogador 1", m};
+    Jogador p2{"Jogador 2", g};
+    // cout << "\nNome: " << p1.get_jogador_nome() << endl << "Classe: " << p1.get_jogador_classe_nome() << endl;
+    // Graveto_do_aprendiz item1;
+    // Espada_enferrujada item2;
 
-    if(p1.mochila_vazia()) cout << "MOCHILA VAZIA\n"; 
-    p1.mochila_inserir(item1);
-    p1.mochila_inserir(item2);
-    p1.mochila_mostrar();
-    p1.mochila_remover(item1);
-    p1.mochila_mostrar();
-    cout << "DANO: " << p1.get_jogador_classe()->ataque();//da pra atacar usando a funcao get_jogador_classe
+    // if(p1.mochila_vazia()) cout << "MOCHILA VAZIA\n"; 
+    // p1.mochila_inserir(item1);
+    // p1.mochila_inserir(item2);
+    // p1.mochila_mostrar();
+    // p1.mochila_remover(item1);
+    // p1.mochila_mostrar();
+    // cout << "DANO: " << p1.get_jogador_classe()->ataque();//da pra atacar usando a funcao get_jogador_classe
+    mostrar_status_batalha(p1, *p2.get_jogador_classe());
 }
--- Funcoes.h
+++ Funcoes.h
@@ -14,21 +14,21 @@ void mostrar_status_entidade(Entidade& entidade){
     cout <<"NOME: " << entidade.get_entidade_nome() << "VIDA: " << entidade.get_vidaBase() << endl << "DEFESA: " << entidade.get_defesaBase() << endl << "ATAQUE: " << entidade.get_ataqueBase();
 }
 
-void mostrar_status_batalha(Entidade& player, Entidade& inimigo){
+void mostrar_status_batalha(Jogador& player, Entidade& inimigo){ //recebe um jogador e um inimigo como parametro
     cout << "\tSEUS STATUS:" << "\t\t\tSTATUS DO INIMIGO:" << endl;
-    cout <<"NOME: " << player.get_entidade_nome() << "\t\tNOME: " << inimigo.get_entidade_nome() << endl;
-    cout <<"VIDA: " << player.get_vidaBase() << "\t\tVIDA: " << inimigo.get_vidaBase() << endl;
-    cout <<"DEFESA: " << player.get_defesaBase() << "\t\tDEFESA: " << inimigo.get_defesaBase() <<  endl;
-    cout <<"ATAQUE: " << player.get_ataqueBase() << "\t\tATAQUE: " << inimigo.get_ataqueBase() << endl;
+    cout <<"NOME: " << player.get_jogador_nome() << "\t\t\tNOME: " << inimigo.get_entidade_nome() << endl;
+    cout <<"VIDA: " << player.get_jogador_classe()->get_vidaBase() << "\t\t\tVIDA: " << inimigo.get_vidaBase() << endl;
+    cout <<"DEFESA: " << player.get_jogador_classe()->get_defesaBase() << "\t\t\tDEFESA: " << inimigo.get_defesaBase() <<  endl;
+    cout <<"ATAQUE: " << player.get_jogador_classe()->get_ataqueBase() << "\t\t\tATAQUE: " << inimigo.get_ataqueBase() << endl;
 }
 
 
-void batalha(Jogador& player, Dungeon& dungeon){
-    bool p_turno = true, d_turno = false;
+void batalha(Jogador& player, Dungeon& dungeon){ 
+    bool p_turno = true, d_turno = false; // o jogador começa jogando 
     int turno = 1;
-    while(player.get_jogador_classe()->get_vidaBase() > 0 || dungeon.get_adversario()->get_vidaBase() > 0){
+    while(player.get_jogador_classe()->get_vidaBase() > 0 && dungeon.get_adversario()->get_vidaBase() > 0){
         cout << "\t\tTURNO: " << turno;
-        mostrar_status_batalha(*player.get_jogador_classe(), *dungeon.get_adversario());
+        mostrar_status_batalha(player, *dungeon.get_adversario());
 
         if(p_turno){
             //o jogador faz as coisas dele aqui
